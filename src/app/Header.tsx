b2fdba1cde27484fc9f373c12e11106501d70989
Gabriel Henrique  Bueno <gabrielhenriquesi1996@gmail.com>
2023-06-09 17:28:15 -0300

    first commit

---
+++ src/app/Header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="bg-gray-900 h-[100px] flex justify-center">
+      <div className="flex items-center gap-4">
+        <div>
+          <Image src="ignitelogo.svg" width={50} height={50} alt="logo" />
+        </div>
+
+        <div>
+          <h1 className="text-white text-2x1 ">Ignite Feed</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/Sidebar.tsx
@@ -0,0 +1,8 @@
+export default function Sidebar() {
+  return (
+    <div>
+      <p className="text-white">Sidebar</p>
+      <h1 className="text-white">teste</h1>;
+    </div>
+  );
+}
